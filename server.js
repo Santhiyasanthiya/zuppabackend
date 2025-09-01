6400d4f560c3355b4f3d14ed9a602d470218f45b
Santhiyasanthiya <santhiya30032@gmail.com>
2025-09-01 13:10:28 +0530

    otp

--- server.js
+++ server.js
@@ -839,7 +839,7 @@ app.post("/api/software-download-forgot-password", async (req, res) => {
     );
 
     // Send reset link via email
-    const resetLink = `${process.env.BASE_URL}/reset-password?token=${resetToken}&email=${encodeURIComponent(
+    const resetLink = `${process.env.BASE_URL}/android_reset/?token=${resetToken}&email=${encodeURIComponent(
       email
     )}`;
 
